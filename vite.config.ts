f77ffe79a20c970e899149b3eaedadf584068f59
Chris Webb <crippit@gmail.com>
2026-02-04 22:07:10 -0500

    v123

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import angular from '@analogjs/vite-plugin-angular';
 export default defineConfig({
   base: '/',
   plugins: [angular()],
-  define: {
-    'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)
-  },
   build: {
     outDir: 'dist',
-  }
+    target: 'esnext',
+  },
 });
