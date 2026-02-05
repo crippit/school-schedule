88d39bfd8a4e206d32ab03e1fb4d9a697f35d6c9
Chris Webb <crippit@gmail.com>
2026-02-04 22:24:34 -0500

    routes

--- app.routes.ts
+++ app.routes.ts
@@ -5,9 +5,10 @@ import { CalendarComponent } from './components/calendar.component';
 import { ReviewComponent } from './components/review.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'setup', pathMatch: 'full' },
+  { path: '', component: SetupComponent, title: 'Setup' },
   { path: 'setup', component: SetupComponent, title: 'Setup' },
   { path: 'classes', component: ClassInputComponent, title: 'Classes' },
   { path: 'calendar', component: CalendarComponent, title: 'Calendar' },
-  { path: 'review', component: ReviewComponent, title: 'Review' }
+  { path: 'review', component: ReviewComponent, title: 'Review' },
+  { path: '**', redirectTo: '' }
 ];
