b0074b305730c973e8b3d1511d37f00fca23a815
Chris Webb <crippit@gmail.com>
2026-02-04 21:58:19 -0500

    gem

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
   base: '/',
-  plugins: [angular()]
+  plugins: [angular()],
+  define: {
+    'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)
+  },
+  build: {
+    outDir: 'dist',
+  }
 });
