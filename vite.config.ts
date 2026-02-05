dd9aea42796da57f2b4b8dae57f9d72a98552eca
Chris Webb <crippit@gmail.com>
2026-02-04 21:23:23 -0500

    base

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
+  base: '/', // Add this line explicitly
   plugins: [angular()],
   define: {
-    // Inject the API Key from the Netlify/Build environment
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
